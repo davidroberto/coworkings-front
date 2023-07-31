dd46f3e84ac80eb11ad46eeb9311827ccff04279
David Robert <contact@david-robert.fr>
2023-07-31 14:35:26 +0200

    delete coworking

--- src/page/CoworkingsPage.jsx
+++ src/page/CoworkingsPage.jsx
@@ -2,20 +2,39 @@ import React, { useEffect, useState } from "react";
 
 const CoworkingsPage = () => {
   const [coworkings, setCoworkings] = useState([]);
+  const [deleteCoworkingMessage, setDeleteCoworkingMessage] = useState(null);
 
   const fetchCoworkings = async () => {
-    const response = await fetch("http://localhost:3010/api/coworkings");
+    const response = await fetch("http://localhost:3010/api/coworkings", {
+      method: "GET",
+    });
     const responseJs = await response.json();
 
     setCoworkings(responseJs.data);
   };
 
+  // useEffect avec la variable deleteCoworkingMessage
+  // dans le tableau, permet de dire qu'on executer
+  // la fonction fetchCoworkings à chaque fois que
+  // la variable deleteCoworkingMessage est modifiée
   useEffect(() => {
     fetchCoworkings();
-  }, []);
+  }, [deleteCoworkingMessage]);
+
+  const handleDeleteCoworking = async (coworkingId) => {
+    const responseDelete = await fetch(`http://localhost:3010/api/coworkings/${coworkingId}`, {
+      method: "DELETE",
+    });
+
+    const responseDeleteJs = await responseDelete.json();
+
+    setDeleteCoworkingMessage(responseDeleteJs.message);
+  };
 
   return (
     <div>
+      <h1>Liste des coworkings</h1>
+      {deleteCoworkingMessage && <p>{deleteCoworkingMessage}</p>}
       {coworkings.map((coworking) => (
         <div key={coworking.id}>
           <h2>{coworking.name}</h2>
@@ -23,6 +42,7 @@ const CoworkingsPage = () => {
             Adresse :{coworking.address.number} {coworking.address.street} - {coworking.address.postcode}
             {coworking.address.city}
           </p>
+          <button onClick={() => handleDeleteCoworking(coworking.id)}>Supprimer le coworking</button>
         </div>
       ))}
     </div>
